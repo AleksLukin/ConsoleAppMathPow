5d7704b868f98f6607f63d639f8260effae97431
ALLUK <lam83@bk.ru>
2023-05-25 22:19:38 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для которого необходимо вычислить его квадрат: ");
+            int mathPow=int.Parse(Console.ReadLine());
+
+            int increment = 1;
+            int result;
+            
+            for (int i = 0; i <= mathPow; i++)
+            {
+                if (mathPow == 1)
+                {
+                    increment = (2 * increment) - 1;
+                }
+                else
+                {
+                    result = (2 * i) - 1; 
+
+                    increment = result +increment;      
+                }
+            }            
+            Console.Write($"Квадрат числа {mathPow} равен: {increment}");
+
+            Console.ReadLine();
+        }
+    }
+}
